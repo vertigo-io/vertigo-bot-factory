d91223ba206663a9d1c41118576f103cddc168c8
skerdudou <steven.kerdudou@gmail.com>
2020-01-29 18:16:00 +0100

    [Chatbot] Change runner conf storage from berkeley to Json in file

--- vertigo-bot-executor/src/main/java/io/vertigo/chatbot/executor/manager/ExecutorConfigManager.java
+++ vertigo-bot-executor/src/main/java/io/vertigo/chatbot/executor/manager/ExecutorConfigManager.java
@@ -1,28 +1,45 @@
 package io.vertigo.chatbot.executor.manager;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import javax.inject.Inject;
 
+import org.apache.commons.io.FileUtils;
+
 import io.vertigo.chatbot.commons.domain.ExecutorConfiguration;
-import io.vertigo.commons.transaction.VTransactionManager;
-import io.vertigo.commons.transaction.VTransactionWritable;
 import io.vertigo.core.component.Activeable;
 import io.vertigo.core.component.Manager;
-import io.vertigo.dynamo.kvstore.KVStoreManager;
+import io.vertigo.core.param.ParamManager;
+import io.vertigo.lang.VSystemException;
+import io.vertigo.vega.engines.webservice.json.JsonEngine;
 
 public class ExecutorConfigManager implements Manager, Activeable {
 
 	@Inject
-	private KVStoreManager kVStoreManager;
+	private ParamManager paramManager;
 
 	@Inject
-	private VTransactionManager transactionManager;
+	private JsonEngine jsonEngine;
 
+	private File configDataFile;
 	private ExecutorConfiguration executorConfiguration;
 
 	@Override
 	public void start() {
-		try (final VTransactionWritable transaction = transactionManager.createCurrentTransaction()) {
-			executorConfiguration = kVStoreManager.find("ExecutorConfig", "executorConfiguration", ExecutorConfiguration.class).orElse(null);
+		final String configDataFilePath = paramManager.getParam("CONFIG_DATA_FILE").getValueAsString();
+		configDataFile = new File(configDataFilePath);
+
+		if (configDataFile.exists() && configDataFile.canRead()) {
+			try {
+				final String json = FileUtils.readFileToString(configDataFile, StandardCharsets.UTF_8);
+				executorConfiguration = jsonEngine.fromJson(json, ExecutorConfiguration.class);
+			} catch (final Exception e) {
+				throw new VSystemException(e, "Error reading parameter file {0}", configDataFilePath);
+			}
+		} else {
+			executorConfiguration = new ExecutorConfiguration();
 		}
 	}
 
@@ -35,7 +52,13 @@ public class ExecutorConfigManager implements Manager, Activeable {
 	public void loadConfig(final ExecutorConfiguration executorConfiguration) {
 		this.executorConfiguration = executorConfiguration;
 
-		kVStoreManager.put("ExecutorConfig", "executorConfiguration", executorConfiguration);
+		final String json = jsonEngine.toJson(executorConfiguration);
+
+		try {
+			FileUtils.writeStringToFile(configDataFile, json, StandardCharsets.UTF_8);
+		} catch (final IOException e) {
+			throw new VSystemException(e, "Error writing parameter file {0}", configDataFile.getPath());
+		}
 	}
 
 	/**
