f643b9da09d50d93a1f1ac3828634825d60d532f
skerdudou <steven.kerdudou@gmail.com>
2020-05-06 13:25:18 +0200

    [Docker] Fix restore script

--- vertigo-bot-docker/dockerCompose/restore.sh
+++ vertigo-bot-docker/dockerCompose/restore.sh
@@ -2,4 +2,4 @@
 
 docker exec -it chatbotfactory_postgres_1 /opt/backup/restore.sh
 
-docker exec chatbotfactory_influxdb_1 /opt/backup/restore.sh
+docker exec -it chatbotfactory_influxdb_1 /opt/backup/restore.sh
