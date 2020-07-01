b07966c561871830e234a9c7c38567b5d3f0e95c
skerdudou <steven.kerdudou@gmail.com>
2020-07-01 19:03:32 +0200

    [Docker] Add DEPLOY_PATH parameter for Designer image
    This parameter change the deploy path of Tomcat webapp and must be
    reflecting actual path from the browser.

--- vertigo-bot-docker/build/Designer/start.sh
+++ vertigo-bot-docker/build/Designer/start.sh
@@ -10,6 +10,11 @@ _term() {
 
 trap _term SIGTERM
 
+# Customize tomcat deploy path from env var DEPLOY_PATH
+rm -f /opt/tomcat/conf/Catalina/localhost/*.xml
+cp /opt/tomcat/conf/Catalina/localhost/designer.xml.ref /opt/tomcat/conf/Catalina/localhost/${DEPLOY_PATH//\//#}.xml
+
+# Start Tomcat
 su -c "/opt/tomcat/bin/startup.sh" tomcat
 
 touch /opt/tomcat/logs/catalina.out
