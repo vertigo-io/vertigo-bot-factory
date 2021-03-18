e9e5e9a57a64437757988288739157dd7e1c12b1
skerdudou <steven.kerdudou@gmail.com>
2021-03-18 19:24:17 +0100

    [AI] Add unit test configuration

---
+++ vertigo-bot-executor/src/test/java/io/vertigo/ai/NluAiTest.java
@@ -0,0 +1,81 @@
+package io.vertigo.ai;
+
+/**
+ * vertigo - simple java starter
+ *
+ * Copyright (C) 2013-2019, Vertigo.io, KleeGroup, direction.technique@kleegroup.com (http://www.kleegroup.com)
+ * KleeGroup, Centre d'affaire la Boursidiere - BP 159 - 92357 Le Plessis Robinson Cedex - France
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.vertigo.ai.nlu.NluManager;
+import io.vertigo.core.node.AutoCloseableNode;
+import io.vertigo.core.node.component.di.DIInjector;
+import io.vertigo.core.node.config.BootConfig;
+import io.vertigo.core.node.config.NodeConfig;
+import io.vertigo.core.param.Param;
+import io.vertigo.core.plugins.resource.classpath.ClassPathResourceResolverPlugin;
+import io.vertigo.core.plugins.resource.url.URLResourceResolverPlugin;
+
+/**
+ * Test of the NLU manager.
+ *
+ * @author skerdudou
+ */
+public final class NluAiTest {
+
+	private AutoCloseableNode node;
+
+	@Inject
+	private NluManager nluManager;
+
+	@Test
+	public void testNlu() {
+		// TODO
+	}
+
+	@BeforeEach
+	public final void setUp() {
+		node = new AutoCloseableNode(buildNodeConfig());
+		DIInjector.injectMembers(this, node.getComponentSpace());
+	}
+
+	@AfterEach
+	public final void tearDown() {
+		if (node != null) {
+			node.close();
+		}
+	}
+
+	private NodeConfig buildNodeConfig() {
+		return NodeConfig.builder()
+				.withBoot(BootConfig.builder()
+						.withLocales("fr_FR")
+						.addPlugin(ClassPathResourceResolverPlugin.class)
+						.addPlugin(URLResourceResolverPlugin.class)
+						.build())
+				.addModule(new AiFeatures()
+						.withNLU()
+						.withRasaNLU(Param.of("rasaUrl", "http://localhost:5005"))
+						.build())
+				.build();
+	}
+
+}
