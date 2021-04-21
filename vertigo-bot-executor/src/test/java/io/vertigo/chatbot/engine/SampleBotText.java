3803fa8ce5ddb601bf74ed6b67c312e4e0b4cc07
skerdudou <steven.kerdudou@gmail.com>
2021-04-21 10:59:25 +0200

    BT text lisibility

--- vertigo-bot-executor/src/test/java/io/vertigo/chatbot/engine/SampleBotText.java
+++ vertigo-bot-executor/src/test/java/io/vertigo/chatbot/engine/SampleBotText.java
@@ -11,8 +11,8 @@ import io.vertigo.ai.bt.command.BtCommandManager;
 import io.vertigo.chatbot.engine.model.BotInput;
 import io.vertigo.chatbot.engine.model.BotResponse;
 import io.vertigo.chatbot.engine.model.BotResponse.BotStatus;
-import io.vertigo.chatbot.engine.plugins.bt.command.bot.BotBtCommandParserPlugin;
 import io.vertigo.chatbot.engine.model.TopicDefinition;
+import io.vertigo.chatbot.engine.plugins.bt.command.bot.BotBtCommandParserPlugin;
 import io.vertigo.commons.CommonsFeatures;
 import io.vertigo.core.node.AutoCloseableNode;
 import io.vertigo.core.node.config.ModuleConfig;
@@ -29,33 +29,33 @@ public class SampleBotText {
 	private static BotEngine botEngine;
 
 	private final String botText = "" +
-			"begin sequence\n" +
-			"	inputString u/name \"Hello I'm Alan what is your name ?\"\n" +
-			"		begin selector\n" +
-			"			eq i/name X\n" +
-			"			begin sequence\n" +
-			"				inputString i/name \"Hi {{u/name}} please select [W]eather, [X]icket, [G]ame or e[X]it ?\" W G X\n" +
-			"				begin selector\n" +
-			"					fulfilled i/done\n" +
-			"					begin switch i/name\n" +
-			"						begin case W\n" +
-			"							inputString w/city \"Please choose a city\"\n" +
-			"							say \"It's sunny in {{w/city}} !\"\n" +
-			"							set i/done ok\n" +
-			"							remove w/*\n" +
-			"						end case\n" +
-			"						say \"Choice not supported yet\"\n" +
-			"						remove i/name\n" +
-			"					end switch\n" +
-			"				end selector\n" +
-			"				inputString rate/rating \"Please rate the response [0, 1, 2, 3, 4, 5]\" 0 1 2 3 4 5\n" +
-			"				say \"You have rated {{rate/rating}}\"\n" +
-			"				remove rate/*\n" +
-			"				remove i/*\n" +
-			"			end sequence\n" +
-			"		end selector\n" +
-			"	say \"bye bye {{u/name}}\"\n" +
-			"end sequence\n";
+			"\n	begin sequence" +
+			"\n		inputString u/name \"Hello I'm Alan what is your name ?\"" +
+			"\n		begin selector" +
+			"\n			eq i/name X" +
+			"\n			begin sequence" +
+			"\n				inputString i/name \"Hi {{u/name}} please select [W]eather, [X]icket, [G]ame or e[X]it ?\" W G X" +
+			"\n				begin selector" +
+			"\n					fulfilled i/done" +
+			"\n					begin switch i/name" +
+			"\n						begin case W" +
+			"\n							inputString w/city \"Please choose a city\"" +
+			"\n							say \"It's sunny in {{w/city}} !\"" +
+			"\n							set i/done ok" +
+			"\n							remove w/*" +
+			"\n						end case" +
+			"\n						say \"Choice not supported yet\"" +
+			"\n						remove i/name" +
+			"\n					end switch" +
+			"\n				end selector" +
+			"\n				inputString rate/rating \"Please rate the response [0, 1, 2, 3, 4, 5]\" 0 1 2 3 4 5" +
+			"\n				say \"You have rated {{rate/rating}}\"" +
+			"\n				remove rate/*" +
+			"\n				remove i/*" +
+			"\n			end sequence" +
+			"\n		end selector" +
+			"\n		say \"bye bye {{u/name}}\"" +
+			"\n	end sequence";
 
 	public static void main(final String[] args) {
 		try (AutoCloseableNode node = new AutoCloseableNode(buildNodeConfig())) {
