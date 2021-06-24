6d72ac60c757b9012b29903082caefb4efb4e590
valérian baillet <valerian.baillet@kleegroup.com>
2021-06-24 10:56:45 +0200

    Version correction

--- vertigo-bot-docker/build/buildDesigner.sh
+++ vertigo-bot-docker/build/buildDesigner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t vertigoio/bot-factory-designer -t vertigoio/bot-factory-designer:0.8.0 -f ./DockerfileDesigner .
+docker build -t vertigoio/bot-factory-designer -t vertigoio/bot-factory-designer:0.5.0 -f ./DockerfileDesigner .
