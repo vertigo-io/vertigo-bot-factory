85d739f1de758b1d8ad004ce481dcaab85672275
vbaillet <77967925+valerian-baillet@users.noreply.github.com>
2021-06-24 10:58:38 +0200

    Recette/dockerfile (#30)
    * Correction of dockerfile
    
    * Add docker file
    
    * Add setenv
    
    * Version correction

--- vertigo-bot-docker/build/Runner/start.sh
+++ vertigo-bot-docker/build/Runner/start.sh
@@ -4,22 +4,13 @@ _term() {
   echo "Caught SIGTERM signal!" 
   
   su -c "/opt/tomcat/bin/shutdown.sh" tomcat
-  service rabbitmq-server stop
   
   exit 0
 }
 
 trap _term SIGTERM
 
-service rabbitmq-server start
-
-while ! nc -z 127.0.0.1 5672;
-do
-  echo waiting rabbitmq to start;
-  sleep 1;
-done;
-echo rabbitmq ok;
-
+# Start Tomcat
 su -c "/opt/tomcat/bin/startup.sh" tomcat
 
 touch /opt/tomcat/logs/catalina.out
