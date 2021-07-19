0c6b41989fa96cee5ed7660ac366aaf8cd2ed97a
valérian baillet <valerian.baillet@kleegroup.com>
2021-07-19 14:12:06 +0200

    Correction analytics
    Add technical measurements for nlu

--- vertigo-bot-executor/src/main/java/io/vertigo/chatbot/analytics/AnalyticsSenderServices.java
+++ vertigo-bot-executor/src/main/java/io/vertigo/chatbot/analytics/AnalyticsSenderServices.java
@@ -22,6 +22,39 @@ public class AnalyticsSenderServices implements Component {
 	@Inject
 	private AnalyticsManager analyticsManager;
 
+	//Analytics keys
+	private static final String DB_KEY = "chatbotmessages";
+	private static final String TEXT_KEY = "text";
+	private static final String TYPE_KEY = "type";
+	private static final String NLU_KEY = "isNlu";
+	private static final String USER_MESSAGE_KEY = "isUserMessage";
+	private static final String CONFIDENCE_KEY = "confidence";
+	private static final String TECHNICAL_KEY = "isTechnical";
+	private static final String FALLBACK_KEY = "isFallback";
+	private static final String SESSION_START_KEY = "isSessionStart";
+
+	//Input types key
+	private static final String BUTTONS_INPUT_KEY = "button";
+	//for the topic {} instructions
+	private static final String SWITCH_INPUT_KEY = "switch";
+	private static final String TECHNICAL_INPUT_KEY = "technical";
+
+	//Training keys
+	private static final String BOT_KEY = "botId";
+	private static final String NODE_KEY = "nodId";
+	private static final String TRAINING_KEY = "traId";
+	private static final String MODEL_KEY = "modelName";
+
+	private static final Double TRUE_BIGDECIMAL = 1D;
+	private static final Double FALSE_BIGDECIMAL = 0D;
+
+	/**
+	 * Send all the events to the events database
+	 *
+	 * @param metadatas data from botResponse
+	 * @param executorConfiguration the executor configuration (i.e. node, bot)
+	 * @param input the user input
+	 */
 	public void sendEventToDb(final Map<String, Object> metadatas, final ExecutorConfiguration executorConfiguration, final BotInput input) {
 		//Get values from response
 		final AnalyticsObjectSend analytics = (AnalyticsObjectSend) metadatas.get(BotEngine.ANALYTICS_KEY);
@@ -34,58 +67,72 @@ public class AnalyticsSenderServices implements Component {
 		if (isNlu) {
 			sendNluEvent(input, codeTopic, accuracy, topicsPast, executorConfiguration);
 		} else {
-			preparePastTopics(topicsPast, executorConfiguration);
+			//Get a button response
+			sendPastTopics(topicsPast, executorConfiguration);
 		}
 	}
 
+	//Send an event with the nlu topics
+	//Send also all the topics passed with topic {} instruction
 	private void sendNluEvent(final BotInput input, final String codeTopic, final Double accuracy, final List<TopicDefinition> topicsPast, final ExecutorConfiguration executorConfiguration) {
-		final AProcessBuilder processBuilder = AProcess.builder("chatbotmessages", codeTopic, Instant.now(), Instant.now()) // timestamp of emitted event
-				.addTag("text", input.getMessage())
-				.addTag("type", "text")
-				.setMeasure("isNlu", 1D)
-				.setMeasure("isUserMessage", 1D)
-				.setMeasure("confidence", accuracy);
+		final AProcessBuilder processBuilder = AProcess.builder(DB_KEY, codeTopic, Instant.now(), Instant.now()) // timestamp of emitted event
+				.addTag(TEXT_KEY, input.getMessage())
+				.addTag(TYPE_KEY, TEXT_KEY)
+				.setMeasure(NLU_KEY, TRUE_BIGDECIMAL)
+				.setMeasure(USER_MESSAGE_KEY, TRUE_BIGDECIMAL)
+				.setMeasure(CONFIDENCE_KEY, accuracy);
+		//test if topic is a fallback topic
 		if (codeTopic.equals(BotEngine.FALLBACK_TOPIC_NAME)) {
-			prepareFallBackEvent(processBuilder, input, codeTopic, executorConfiguration, accuracy);
+			prepareFallBackEvent(processBuilder);
+		} else {
+			processBuilder.setMeasure(TECHNICAL_KEY, FALSE_BIGDECIMAL);
 		}
-		preparePastTopics(topicsPast, executorConfiguration);
+		sendPastTopics(topicsPast, executorConfiguration);
 		setConfigurationInformation(processBuilder, executorConfiguration);
 		analyticsManager.addProcess(processBuilder.build());
 	}
 
-	private static void prepareFallBackEvent(final AProcessBuilder builder, final BotInput input, final String codeTopic, final ExecutorConfiguration executorConfiguration, final Double accuracy) {
+	private static void prepareFallBackEvent(final AProcessBuilder builder) {
 		builder
-				.setMeasure("isTechnical", 1D)
-				.setMeasure("isFallback", 1D);
+				.setMeasure(TECHNICAL_KEY, TRUE_BIGDECIMAL)
+				.setMeasure(FALLBACK_KEY, TRUE_BIGDECIMAL);
 
 	}
 
-	private void preparePastTopics(final List<TopicDefinition> topicsPast, final ExecutorConfiguration executorConfiguration) {
+	//Send all the topics passed during the sequence in BT
+	private void sendPastTopics(final List<TopicDefinition> topicsPast, final ExecutorConfiguration executorConfiguration) {
+		boolean isFirst = true;
+		//Can't have fallback instructions
 		for (TopicDefinition topic : topicsPast) {
 			//Create the measurements
 			final String codeTopic = topic.getCode();
-			final boolean isFallback = codeTopic.equals(BotEngine.FALLBACK_TOPIC_NAME);
 			final boolean isEnd = codeTopic.equals(BotEngine.END_TOPIC_NAME);
 
-			final AProcessBuilder processBuilder = AProcess.builder("chatbotmessages", topic.getCode(), Instant.now(), Instant.now()) // timestamp of emitted event
-					.addTag("text", "")
-					.addTag("type", "text")
-					.setMeasure("isTechnical", isTechnical(isFallback, isEnd))
-					.setMeasure("confidence", 1D);
+			final AProcessBuilder processBuilder = AProcess.builder(DB_KEY, topic.getCode(), Instant.now(), Instant.now()) // timestamp of emitted event
+					.addTag(TEXT_KEY, "")
+					.addTag(TYPE_KEY, isFirst ? BUTTONS_INPUT_KEY : SWITCH_INPUT_KEY)
+					.setMeasure(TECHNICAL_KEY, isEnd ? TRUE_BIGDECIMAL : FALSE_BIGDECIMAL)
+					.setMeasure(CONFIDENCE_KEY, TRUE_BIGDECIMAL);
 			setConfigurationInformation(processBuilder, executorConfiguration);
 			analyticsManager.addProcess(processBuilder.build());
+			isFirst = false;
 		}
 
 	}
 
+	/**
+	 * Set the event start topic to eventdb
+	 *
+	 * @param executorConfiguration the executor configuration (i.e. node, bot etc...)
+	 */
 	public void sendEventStartToDb(final ExecutorConfiguration executorConfiguration) {
 		//Create the process
-		final AProcessBuilder processBuilder = AProcess.builder("chatbotmessages", BotEngine.START_TOPIC_NAME, Instant.now(), Instant.now()) // timestamp of emitted event
-				.addTag("text", "")
-				.addTag("type", "technical")
-				.setMeasure("isSessionStart", 1D)
-				.setMeasure("confidence", 1D)
-				.setMeasure("isTechnical", 1D);
+		final AProcessBuilder processBuilder = AProcess.builder(DB_KEY, BotEngine.START_TOPIC_NAME, Instant.now(), Instant.now()) // timestamp of emitted event
+				.addTag(TEXT_KEY, "")
+				.addTag(TYPE_KEY, TECHNICAL_INPUT_KEY)
+				.setMeasure(SESSION_START_KEY, TRUE_BIGDECIMAL)
+				.setMeasure(CONFIDENCE_KEY, TRUE_BIGDECIMAL)
+				.setMeasure(TECHNICAL_KEY, TRUE_BIGDECIMAL);
 
 		setConfigurationInformation(processBuilder, executorConfiguration);
 
@@ -94,14 +141,10 @@ public class AnalyticsSenderServices implements Component {
 
 	private static void setConfigurationInformation(final AProcessBuilder builder, final ExecutorConfiguration executorConfiguration) {
 		builder
-				.addTag("botId", String.valueOf(executorConfiguration.getBotId()))
-				.addTag("nodId", String.valueOf(executorConfiguration.getNodId()))
-				.addTag("traId", String.valueOf(executorConfiguration.getTraId()))
-				.addTag("modelName", String.valueOf(executorConfiguration.getModelName()));
+				.addTag(BOT_KEY, String.valueOf(executorConfiguration.getBotId()))
+				.addTag(NODE_KEY, String.valueOf(executorConfiguration.getNodId()))
+				.addTag(TRAINING_KEY, String.valueOf(executorConfiguration.getTraId()))
+				.addTag(MODEL_KEY, String.valueOf(executorConfiguration.getModelName()));
 	}
 
-	//return 1D if topic is technical
-	private static double isTechnical(final boolean isFallback, final boolean isEnd) {
-		return isFallback || isEnd ? 1D : 0D;
-	}
 }
