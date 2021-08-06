ba61a7afa269a706b68cb45f024d9d99830ddfcd
valérian Baillet <valerian.baillet@kleegroup.com>
2021-08-06 15:14:29 +0200

    Add architecture for POC Jira

---
+++ vertigo-bot-extensions/vertigo-bot-jira/src/main/java/io/vertigo/chatbot/engine/plugins/bt/jira/command/bot/BotJiraNodeProvider.java
@@ -0,0 +1,11 @@
+package io.vertigo.chatbot.engine.plugins.bt.jira.command.bot;
+
+import io.vertigo.core.node.component.Component;
+
+public class BotJiraNodeProvider implements Component {
+
+	public BotJiraNodeProvider() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
