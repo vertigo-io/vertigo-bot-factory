a27e638ae903ed537be4ffad066c42f50acc333c
valérian Baillet <valerian.baillet@kleegroup.com>
2021-08-06 15:25:55 +0200

    Add structure and service

--- vertigo-bot-extensions/vertigo-bot-jira/src/main/java/io/vertigo/chatbot/engine/plugins/bt/jira/command/bot/BotJiraNodeProvider.java
+++ vertigo-bot-extensions/vertigo-bot-jira/src/main/java/io/vertigo/chatbot/engine/plugins/bt/jira/command/bot/BotJiraNodeProvider.java
@@ -1,11 +1,29 @@
 package io.vertigo.chatbot.engine.plugins.bt.jira.command.bot;
 
+import static io.vertigo.ai.bt.BTNodes.sequence;
+import static io.vertigo.chatbot.engine.plugins.bt.command.bot.BotNodeProvider.inputString;
+
+import javax.inject.Inject;
+
+import io.vertigo.ai.bb.BlackBoard;
+import io.vertigo.ai.bt.BTNode;
+import io.vertigo.ai.bt.BTStatus;
+import io.vertigo.chatbot.engine.plugins.bt.jira.impl.JiraService;
 import io.vertigo.core.node.component.Component;
 
 public class BotJiraNodeProvider implements Component {
 
-	public BotJiraNodeProvider() {
-		// TODO Auto-generated constructor stub
+	@Inject
+	private JiraService jiraService;
+
+	public BTNode jiraIssueCreation(final BlackBoard bb) {
+		return sequence(
+				inputString(bb, "/user/local/jira/user", "Quel est votre nom utilisateur"),
+				() -> {
+					jiraService.createRequestJira();
+
+					return BTStatus.Succeeded;
+				});
 	}
 
 }
