769f6ecceb02e7225b2c7537edd1207bab55e9a9
valérian Baillet <valerian.baillet@kleegroup.com>
2021-08-11 09:54:28 +0200

    WIP

--- vertigo-bot-extensions/vertigo-bot-jira/src/main/java/io/vertigo/chatbot/engine/plugins/bt/jira/impl/WebService.java
+++ vertigo-bot-extensions/vertigo-bot-jira/src/main/java/io/vertigo/chatbot/engine/plugins/bt/jira/impl/WebService.java
@@ -1,6 +1,7 @@
 package io.vertigo.chatbot.engine.plugins.bt.jira.impl;
 
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
@@ -20,11 +21,17 @@ import io.vertigo.core.param.ParamManager;
 public class WebService implements Component, Activeable {
 
 	private String url;
+	private String urlTarifReduit;
+	private String user;
+	private String pwd;
 
 	@Override
 	public void start() {
 		var paramManager = Node.getNode().getComponentSpace().resolve(ParamManager.class);
 		url = paramManager.getParam("WEB_SERVICE").getValueAsString();
+		urlTarifReduit = "http://comu-usi-batch-rec:8001/sigRest/v1/customers/33263038/summary";
+		user = "Admin SIG";
+		pwd = "test";
 	}
 
 	@Override
@@ -47,6 +54,39 @@ public class WebService implements Component, Activeable {
 			e.printStackTrace();
 		}
 		return result;
+	}
+
+	public boolean getIsErrorTarifReduit(final String clientNumber) {
+		Map<String, String> headers = Map.of("AuthorizationSIG", JiraHttpRequestHelper.basicAuth(user, pwd),
+				"Content-type", "application/json");
+		final String json = "{" +
+				"\"searchLevel\": 1," +
+				"\"searches\":" +
+				"[{  \"holderPayerInd\":2," +
+				"\"productCodes\": [5]," +
+				"\"states\":[3]" +
+				"}]" +
+
+				"}";
+
+		final HttpRequest request = JiraHttpRequestHelper.createPostRequest(urlTarifReduit, headers, BodyPublishers.ofString(json));
+		final HttpResponse<String> response = JiraHttpRequestHelper.sendRequest(null, request, BodyHandlers.ofString(), 200);
+		final String body = response.body();
+		return testBody(body);
+	}
+
+	private boolean testBody(final String body) {
+		try {
+			final ObjectMapper mapper = new ObjectMapper();
+			final JsonNode results = mapper.readTree(body);
+			for (JsonNode node : results) {
+				final String test = node.at("/contract/currentProfile").asText();
+				final String test2 = "juste pour l'info d'avant";
+			}
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 }
