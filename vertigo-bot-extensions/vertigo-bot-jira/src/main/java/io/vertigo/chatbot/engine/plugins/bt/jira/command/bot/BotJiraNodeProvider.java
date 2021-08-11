4c67ffbbb0d6203e662362ce6ca02ed921052ef4
vprima <victor.prima@kleegroup.fr>
2021-08-11 13:02:38 +0200

    poc_jira_composants

--- vertigo-bot-extensions/vertigo-bot-jira/src/main/java/io/vertigo/chatbot/engine/plugins/bt/jira/command/bot/BotJiraNodeProvider.java
+++ vertigo-bot-extensions/vertigo-bot-jira/src/main/java/io/vertigo/chatbot/engine/plugins/bt/jira/command/bot/BotJiraNodeProvider.java
@@ -5,14 +5,18 @@ import static io.vertigo.ai.bt.BTNodes.sequence;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
+
+import com.atlassian.jira.rest.client.api.domain.BasicComponent;
 
 import io.vertigo.ai.bb.BBKey;
 import io.vertigo.ai.bb.BlackBoard;
 import io.vertigo.ai.bt.BTNode;
 import io.vertigo.ai.bt.BTStatus;
 import io.vertigo.chatbot.engine.BotEngine;
+import io.vertigo.chatbot.engine.model.choice.BotButton;
 import io.vertigo.chatbot.engine.plugins.bt.command.bot.BotNodeProvider;
 import io.vertigo.chatbot.engine.plugins.bt.jira.impl.JiraServerService;
 import io.vertigo.chatbot.engine.plugins.bt.jira.impl.WebService;
@@ -28,9 +32,9 @@ public class BotJiraNodeProvider implements Component {
 	@Inject
 	private WebService webServices;
 
-public BTNode jiraIssueCreation(final BlackBoard bb, final List<String> jfStrings, final String urlSentence) {
+	public BTNode jiraIssueCreation(final BlackBoard bb, final List<String> jfStrings, final String urlSentence) {
 		return () -> {
-			List<String> versions = webServices.getAllVersions();
+			final List<String> versions = webServices.getAllVersions();
 			final String result = jiraService.createIssueJiraCommand(jfStrings, versions);
 			bb.listPush(BotEngine.BOT_RESPONSE_KEY, urlSentence + " " + result);
 			return BTStatus.Succeeded;
@@ -45,10 +49,22 @@ public BTNode jiraIssueCreation(final BlackBoard bb, final List<String> jfString
 			sequence.add(BotNodeProvider.inputString(bb, jiraField.getKey(), jiraField.getQuestion(), validator));
 			jfStrings.add(bb.getString(BBKey.of(jiraField.getKey())));
 		}
+		sequence.add(getComponentIssue(bb, "/user/local/components", "Quel est le composant ?"));
+		jfStrings.add(bb.getString(BBKey.of("/user/local/components")));
 		sequence.add(jiraIssueCreation(bb, jfStrings, urlSentence));
 
 		return sequence(sequence);
 
+	}
+
+	private BTNode getComponentIssue(final BlackBoard bb, final String keyTemplate, final String question) {
+		final List<BasicComponent> listComponents = (List<BasicComponent>) jiraService.getProject().getComponents();
+		final List<BotButton> listButtons = listComponents.stream().map(x -> mapComponentToButtonNode(x)).collect(Collectors.toList());
+		return BotNodeProvider.chooseButton(bb, keyTemplate, question, listButtons);
+	}
+
+	private BotButton mapComponentToButtonNode(final BasicComponent component) {
+		return new BotButton(component.getName(), component.getName());
 	}
 
 }
