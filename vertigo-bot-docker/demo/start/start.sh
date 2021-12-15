d36a058cbfd2290f4b9967446f4e93114be69933
cedricMarechal <cedric.marechal@kleegroup.com>
2021-12-15 11:04:52 +0100

    review 2

--- vertigo-bot-docker/demo/start/start.sh
+++ vertigo-bot-docker/demo/start/start.sh
@@ -2,9 +2,9 @@
 
 rm /usr/local/apache2/htdocs/*
 cp -a /usr/local/tmp/html/. /usr/local/apache2/htdocs/
-sed -i "s|RUNNER_URL|$1|g" /usr/local/apache2/htdocs/index.html
-sed -i "s|RUNNER_URL|$1|g" /usr/local/apache2/htdocs/page2.html
-sed -i "s|BOT_IHM_BASE_URL|$2|g" /usr/local/apache2/htdocs/index.html
-sed -i "s|BOT_IHM_BASE_URL|$2|g" /usr/local/apache2/htdocs/page2.html
+sed -i "s|RUNNER_URL|$RUNNER_URL_ENV|g" /usr/local/apache2/htdocs/index.html
+sed -i "s|RUNNER_URL|$RUNNER_URL_ENV|g" /usr/local/apache2/htdocs/page2.html
+sed -i "s|BOT_IHM_BASE_URL|$BOT_IHM_BASE_URL_ENV|g" /usr/local/apache2/htdocs/index.html
+sed -i "s|BOT_IHM_BASE_URL|$BOT_IHM_BASE_URL_ENV|g" /usr/local/apache2/htdocs/page2.html
 
-apachectl -D FOREGROUND
+httpd-foreground
