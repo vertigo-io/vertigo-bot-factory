a25d7bbe0a7cd4cd6798ca006e632c8c90d18e80
cedricMarechal <93602617+cedricMarechal@users.noreply.github.com>
2022-03-18 11:06:20 +0100

    CHATBOT-400 - Mail grammar improvement (#174)

--- vertigo-bot-executor/src/main/java/io/vertigo/chatbot/executor/mail/BtNodeMailProvider.java
+++ vertigo-bot-executor/src/main/java/io/vertigo/chatbot/executor/mail/BtNodeMailProvider.java
@@ -4,15 +4,17 @@ import io.vertigo.ai.bb.BBKey;
 import io.vertigo.ai.bb.BlackBoard;
 import io.vertigo.ai.bt.BTNode;
 import io.vertigo.ai.bt.BTStatus;
+import io.vertigo.core.lang.Assertion;
 import io.vertigo.core.node.component.Component;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
 import javax.mail.MessagingException;
-
-import static io.vertigo.ai.bt.BTNodes.sequence;
-import static io.vertigo.chatbot.engine.plugins.bt.command.bot.BotNodeProvider.inputString;
+import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class BtNodeMailProvider implements Component {
 
@@ -21,23 +23,28 @@ public class BtNodeMailProvider implements Component {
 
 	private static final Logger LOGGER = LogManager.getLogger(BtNodeMailProvider.class);
 
-	public BTNode sendMail(final BlackBoard bb,
-						   String destinationKey, String destinationQuestion,
-						   String subjectKey, String subjectQuestion,
-						   String messageBodyKey, String messageQuestion) {
-		return sequence (
-			inputString(bb, destinationKey, destinationQuestion),
-			inputString(bb, subjectKey, subjectQuestion),
-			inputString(bb, messageBodyKey, messageQuestion),
-			() -> {
+	public BTNode sendMail(final BlackBoard bb, String subjectKey, String messageBodyKey, Optional<String> attachmentKey, String[] destinationKeys) {
+		return () -> {
+				Optional<FileDescriptor> optFileDescriptor = Optional.empty();
+				String recipients = Arrays.stream(destinationKeys).map(dest -> bb.getString(BBKey.of(dest))).collect(Collectors.joining(","));
+				if (attachmentKey.isPresent()) {
+					FileDescriptor fileDescriptor = new FileDescriptor();
+					BBKey rootFileKey = BBKey.of(attachmentKey.get());
+					fileDescriptor.setFileName(bb.getString(BBKey.of(rootFileKey, "/filename")));
+					String[] fileData = bb.getString(BBKey.of(rootFileKey, "/filecontent")).split(",");
+					Assertion.check().isTrue(fileData.length == 2, "Attachment " + fileDescriptor.getFileName() + " is not Base64 encoded");
+					fileDescriptor.setFileContent(fileData[1]);
+					fileDescriptor.setFileType(URLConnection.getFileNameMap().getContentTypeFor(fileDescriptor.getFileName()));
+					optFileDescriptor = Optional.of(fileDescriptor);
+				}
 				try {
-					mailService.sendMail(bb.getString(BBKey.of(destinationKey)), bb.getString(BBKey.of(subjectKey)), bb.getString(BBKey.of(messageBodyKey)));
+					mailService.sendMail(recipients, bb.getString(BBKey.of(subjectKey)), bb.getString(BBKey.of(messageBodyKey)), optFileDescriptor);
 					return BTStatus.Succeeded;
 				} catch (MessagingException messagingException) {
-					LOGGER.error("Error when sending mail to " + bb.getString(BBKey.of(destinationKey)) + " with mail subject " + bb.getString(BBKey.of(subjectKey)), messagingException);
+					LOGGER.error("Error when sending mail to " + recipients + " with mail subject " + bb.getString(BBKey.of(subjectKey)), messagingException);
 					return BTStatus.Failed;
 				}
-			}
-		);
+			};
+
 	}
 }
