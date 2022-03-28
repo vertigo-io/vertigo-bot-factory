24afa488ded8bff2bf4c617a89674cef4fc35fb0
cedricMarechal <cedric.marechal@kleegroup.com>
2022-03-28 11:04:34 +0200

    CHATBOT-407 - Add clamav antivirus scan

--- vertigo-bot-designer/src/main/java/io/vertigo/chatbot/commons/multilingual/attachment/AttachmentMultilingualResources.java
+++ vertigo-bot-designer/src/main/java/io/vertigo/chatbot/commons/multilingual/attachment/AttachmentMultilingualResources.java
@@ -5,5 +5,7 @@ import io.vertigo.core.locale.MessageKey;
 public enum AttachmentMultilingualResources implements MessageKey {
 
 	MUST_CONTAINS_A_FILE,
-	MAX_TOTAL_SIZE_EXCEEDED
+	MAX_TOTAL_SIZE_EXCEEDED,
+	VIRUSES_FOUND,
+	COULD_NOT_OPEN_FILE_ANTIVIRUS
 }
--- vertigo-bot-designer/src/main/java/io/vertigo/chatbot/designer/builder/services/bot/AttachmentServices.java
+++ vertigo-bot-designer/src/main/java/io/vertigo/chatbot/designer/builder/services/bot/AttachmentServices.java
@@ -8,6 +8,7 @@ import io.vertigo.chatbot.commons.domain.AttachmentExport;
 import io.vertigo.chatbot.commons.domain.AttachmentFileInfo;
 import io.vertigo.chatbot.commons.domain.Chatbot;
 import io.vertigo.chatbot.commons.multilingual.attachment.AttachmentMultilingualResources;
+import io.vertigo.chatbot.designer.builder.services.AntivirusServices;
 import io.vertigo.chatbot.designer.commons.services.FileServices;
 import io.vertigo.chatbot.domain.DtDefinitions;
 import io.vertigo.commons.transaction.Transactional;
@@ -19,12 +20,16 @@ import io.vertigo.datamodel.structure.model.DtListState;
 import io.vertigo.datamodel.structure.util.VCollectors;
 import io.vertigo.datastore.filestore.model.FileInfoURI;
 import io.vertigo.datastore.filestore.model.VFile;
+import xyz.capybara.clamav.commands.scan.result.ScanResult;
 
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Base64;
+import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static io.vertigo.chatbot.designer.utils.ListUtils.MAX_ELEMENTS_PLUS_ONE;
 
@@ -36,6 +41,9 @@ public class AttachmentServices implements Component {
 
 	@Inject
 	private FileServices fileServices;
+
+	@Inject
+	private AntivirusServices antivirusServices;
 
 	public Attachment findById(final long attachmentId) {
 		return attachmentDAO.get(attachmentId);
@@ -56,6 +64,19 @@ public class AttachmentServices implements Component {
 				oldAttachmentFileId = oldAttachment.getAttFiId();
 			}
 			VFile newAttachmentFile = fileServices.getFileTmp(optFileInfoURI.get());
+
+			try {
+				ScanResult result = antivirusServices.checkForViruses(newAttachmentFile.createInputStream());
+				if (result instanceof ScanResult.VirusFound) {
+					Map<String, Collection<String>> virusesMap = ((ScanResult.VirusFound) result).getFoundViruses();
+					String viruses = virusesMap.values().stream()
+							.flatMap(Collection::stream).collect(Collectors.joining(","));
+					throw new VUserException(AttachmentMultilingualResources.VIRUSES_FOUND, viruses);
+				}
+			} catch (IOException ioException) {
+				throw new VUserException(AttachmentMultilingualResources.COULD_NOT_OPEN_FILE_ANTIVIRUS, newAttachmentFile.getFileName(), ioException);
+			}
+
 			if (isMaxSizeExceeded(maxSize, attachmentTotalSize, oldAttachmentSize, newAttachmentFile.getLength())) {
 				throw new VUserException(AttachmentMultilingualResources.MAX_TOTAL_SIZE_EXCEEDED, maxSize);
 			}
