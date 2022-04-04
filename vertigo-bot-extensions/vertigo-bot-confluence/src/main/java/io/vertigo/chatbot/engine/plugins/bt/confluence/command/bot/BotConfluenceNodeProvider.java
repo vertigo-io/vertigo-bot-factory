6c35a9bf3354a7c79378c573fdca98108eb3f48f
cedricMarechal <cedric.marechal@kleegroup.com>
2022-04-04 14:30:02 +0200

    CHATBOT-416 - Add confluence search without user input required

--- vertigo-bot-extensions/vertigo-bot-confluence/src/main/java/io/vertigo/chatbot/engine/plugins/bt/confluence/command/bot/BotConfluenceNodeProvider.java
+++ vertigo-bot-extensions/vertigo-bot-confluence/src/main/java/io/vertigo/chatbot/engine/plugins/bt/confluence/command/bot/BotConfluenceNodeProvider.java
@@ -1,12 +1,5 @@
 package io.vertigo.chatbot.engine.plugins.bt.confluence.command.bot;
 
-import static io.vertigo.ai.bt.BTNodes.sequence;
-import static io.vertigo.chatbot.engine.plugins.bt.command.bot.BotNodeProvider.inputString;
-
-import java.util.List;
-
-import javax.inject.Inject;
-
 import io.vertigo.ai.bb.BBKey;
 import io.vertigo.ai.bb.BBKeyPattern;
 import io.vertigo.ai.bb.BlackBoard;
@@ -16,6 +9,13 @@ import io.vertigo.chatbot.engine.BotEngine;
 import io.vertigo.chatbot.engine.plugins.bt.command.bot.BotNodeProvider;
 import io.vertigo.chatbot.engine.plugins.bt.confluence.impl.ConfluenceServerServices;
 import io.vertigo.core.node.component.Component;
+
+import javax.inject.Inject;
+import java.util.List;
+
+import static io.vertigo.ai.bt.BTNodes.sequence;
+import static io.vertigo.chatbot.engine.plugins.bt.command.bot.BotNodeProvider.inputString;
+import static io.vertigo.chatbot.engine.plugins.bt.command.bot.BotNodeProvider.say;
 
 public final class BotConfluenceNodeProvider implements Component {
 
@@ -35,6 +35,22 @@ public final class BotConfluenceNodeProvider implements Component {
 							bb.listPush(BotEngine.BOT_RESPONSE_KEY, result);
 						}
 						bb.delete(BBKeyPattern.of(keyTemplate));
+						return BTStatus.Succeeded;
+					}
+					return BotNodeProvider.switchTopic(bb, topicFallbackConfluence).eval();
+				});
+
+	}
+
+	public BTNode confluenceAutomaticSearch(final BlackBoard bb, final String introductionSentence, final String topicFallbackConfluence, final String[] keyWords) {
+		return sequence(
+				say(bb, introductionSentence),
+				() -> {
+					final List<String> searchResult = confluenceServerService.searchOnConfluenceCommand(String.join(" ", keyWords));
+					if (!searchResult.isEmpty()) {
+						for (final String result : searchResult) {
+							bb.listPush(BotEngine.BOT_RESPONSE_KEY, result);
+						}
 						return BTStatus.Succeeded;
 					}
 					return BotNodeProvider.switchTopic(bb, topicFallbackConfluence).eval();
