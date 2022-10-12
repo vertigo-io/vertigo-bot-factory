59e67be69e8aa61fa8892f2017b070d7892d825c
cedricMarechal <cedric.marechal@kleegroup.com>
2022-10-12 10:06:22 +0200

    CHATBOT-479 - Fix typo on emoji popup

--- vertigo-bot-designer/src/main/webapp/static/js/c-richtext.js
+++ vertigo-bot-designer/src/main/webapp/static/js/c-richtext.js
@@ -136,7 +136,7 @@ Vue.component('c-richtext', {
 			<q-dialog ref="newEmoji" id="newEmoji" >
 			 	<q-card>
 					<q-card-section>
-						<div class="text-h6" >{{locale == 'fr_FR' ? 'Ajouter une emoji' : 'Add emoji'}}</div>
+						<div class="text-h6" >{{locale == 'fr_FR' ? 'Ajouter un emoji' : 'Add emoji'}}</div>
 					</q-card-section>
 					<q-card-section>
 						<emoji-picker></emoji-picker>
