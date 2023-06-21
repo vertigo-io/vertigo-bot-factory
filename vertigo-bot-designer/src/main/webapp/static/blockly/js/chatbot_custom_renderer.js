7b51a642c9c67d3e0f00f072eb831f2dd06c51e6
cflandre-kleegroup <corentin.flandre@kleegroup.com>
2023-06-21 17:17:56 +0200

    [Decision Tree Tool] Modification of the blockly rendering engine and custom theme

--- vertigo-bot-designer/src/main/webapp/static/blockly/js/chatbot_custom_renderer.js
+++ vertigo-bot-designer/src/main/webapp/static/blockly/js/chatbot_custom_renderer.js
@@ -16,45 +16,15 @@ class CustomConstantProvider extends Blockly.zelos.ConstantProvider{
     constructor() {
         super();
 
-        this.FIELD_TEXT_FONTSIZE = 10   ;
-        this.EMPTY_STATEMENT_INPUT_HEIGHT = 80;
-        // Override a few properties.
-        // /**
-        //  * The width of the notch used for previous and next connections.
-        //  * @type {number}
-        //  * @override
-        //  */
-        // this.NOTCH_WIDTH = 20;
-        //
-        // /**
-        //  * The height of the notch used for previous and next connections.
-        //  * @type {number}
-        //  * @override
-        //  */
-        // this.NOTCH_HEIGHT = 10;
-        //
-        // /**
-        //  * Rounded corner radius.
-        //  * @type {number}
-        //  * @override
-        //  */
-        // this.CORNER_RADIUS = 2;
-        //
-        // /**
-        //  * The height of the puzzle tab used for input and output connections.
-        //  * @type {number}
-        //  * @override
-        //  */
-        // this.TAB_HEIGHT = 8;
-    }
+        // FIELD Renderer
+        this.FIELD_BORDER_RECT_Y_PADDING = 3;
+        this.FIELD_TEXT_FONTSIZE = 12;
 
-    // init() {
-    //     /**
-    //      * An object containing sizing and path information about collapsed block
-    //      * indicators.
-    //      */
-    //     // this.JAGGED_TEETH = this.makeJaggedTeeth();
-    // }
+        // INPUT STATEMENT Renderer
+        this.STATEMENT_INPUT_PADDING_LEFT = 60;
+        this.EMPTY_STATEMENT_INPUT_HEIGHT = 30;
+
+    }
 }
 
 Blockly.blockRendering.register('custom_renderer', CustomRenderer);
