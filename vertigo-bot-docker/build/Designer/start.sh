533babfc2048a4ca0e9f02bbe11db2a71ead1c80
cedricMarechal <cedric.marechal@kleegroup.com>
2023-10-20 16:49:10 +0200

    Nouvelle image de base docker avec tomcat inclut

--- vertigo-bot-docker/build/Designer/start.sh
+++ vertigo-bot-docker/build/Designer/start.sh
@@ -3,7 +3,7 @@
 _term() { 
   echo "Caught SIGTERM signal!" 
   
-  su -c "/opt/tomcat/bin/shutdown.sh" tomcat
+  su -c "/usr/local/tomcat/bin/shutdown.sh" tomcat
   
   exit 0
 }
@@ -11,12 +11,12 @@ _term() {
 trap _term SIGTERM
 
 # Customize tomcat deploy path from env var DEPLOY_PATH (note '//\//#' replace all '/' with '#' for tomcat context name convention)
-rm -f /opt/tomcat/conf/Catalina/localhost/*.xml
-cp /opt/tomcat/conf/Catalina/localhost/designer.xml.ref /opt/tomcat/conf/Catalina/localhost/${DEPLOY_PATH//\//#}.xml
+rm -f /usr/local/tomcat/conf/Catalina/localhost/*.xml
+cp /usr/local/tomcat/conf/Catalina/localhost/designer.xml.ref /usr/local/tomcat/conf/Catalina/localhost/${DEPLOY_PATH//\//#}.xml
 
 # Start Tomcat
-su -c "/opt/tomcat/bin/startup.sh" tomcat
+su -c "/usr/local/tomcat/bin/startup.sh" tomcat
 
-touch /opt/tomcat/logs/catalina.out
-tail -f /opt/tomcat/logs/catalina.out & # just to keep the process alive for docker, also, tomcat logs are visible on container logs
+touch /usr/local/tomcat/logs/catalina.out
+tail -f /usr/local/tomcat/logs/catalina.out & # just to keep the process alive for docker, also, tomcat logs are visible on container logs
 wait "$!" # wait command permit SIGTERM handling
