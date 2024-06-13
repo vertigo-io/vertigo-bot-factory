1d4074e710064f8ceefb53674504dd0f91c7f561
rsaintpaul <romaric.saintpaul@kleegroup.com>
2024-06-13 15:56:34 +0200

    [CHATBOT 610] Deleting sql script duplicate

--- vertigo-bot-designer/src/main/resources/sql/v_2_0/alter_24_05_22.sql
+++
@@ -1,8 +0,0 @@
-alter table SAVED_TRAINING
-    add column ATT_FILE_INFO_ID numeric,
-    add constraint FK_A_SAVED_TRAINING_ATTACHMENT_FILE_INFO foreign key (ATT_FILE_INFO_ID)
-        references ATTACHMENT_FILE_INFO (ATT_FI_ID);
-
-create index A_SAVED_TRAINING_ATTACHMENT_FILE_INFO_FK on SAVED_TRAINING (ATT_FILE_INFO_ID asc);
-
-comment on column SAVED_TRAINING.ATT_FILE_INFO_ID is 'Attachment File Info';
