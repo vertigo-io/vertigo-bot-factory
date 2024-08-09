f240cb5043656cd2cf6c517485ec65f810785990
rsaintpaul <romaric.saintpaul@kleegroup.com>
2024-08-09 11:05:32 +0200

    [CHATBOT-629 create documentDetail page and /new /edit and /delete functionalities]

--- vertigo-bot-designer/src/main/java/io/vertigo/chatbot/designer/builder/services/DocumentaryResourceServices.java
+++ vertigo-bot-designer/src/main/java/io/vertigo/chatbot/designer/builder/services/DocumentaryResourceServices.java
@@ -2,7 +2,10 @@ package io.vertigo.chatbot.designer.builder.services;
 
 import javax.inject.Inject;
 
+import io.vertigo.account.authorization.annotations.Secured;
+import io.vertigo.account.authorization.annotations.SecuredOperation;
 import io.vertigo.chatbot.commons.domain.Chatbot;
+import io.vertigo.chatbot.designer.builder.services.bot.AttachmentServices;
 import io.vertigo.chatbot.designer.dao.DocumentaryResourceDAO;
 import io.vertigo.chatbot.designer.domain.DocumentaryResource;
 import io.vertigo.chatbot.domain.DtDefinitions;
@@ -12,17 +15,41 @@ import io.vertigo.datamodel.criteria.Criterions;
 import io.vertigo.datamodel.structure.model.DtList;
 import io.vertigo.datamodel.structure.model.DtListState;
 
+import static io.vertigo.chatbot.designer.utils.ListUtils.MAX_ELEMENTS_PLUS_ONE;
+
 @Transactional
 public class DocumentaryResourceServices implements Component {
 
     @Inject
     DocumentaryResourceDAO documentaryResourceDAO;
 
+    @Inject
+    AttachmentServices attachmentServices;
+
     public DocumentaryResource getDocResById(final Long  dreId) {
         return documentaryResourceDAO.get(dreId);
     }
 
     public DtList<DocumentaryResource> getAllDocResByBot(final Chatbot bot) {
-        return documentaryResourceDAO.findAll(Criterions.isEqualTo(DtDefinitions.DocumentaryResourceFields.botId, bot.getBotId()), DtListState.of(null));
+        return documentaryResourceDAO.findAll(Criterions.isEqualTo(DtDefinitions.DocumentaryResourceFields.botId, bot.getBotId()), DtListState.of(MAX_ELEMENTS_PLUS_ONE));
+    }
+
+    @Secured("BotUser")
+    public void saveDocumentaryResource(@SecuredOperation("botAdm") final Chatbot bot, final DocumentaryResource documentaryResource) {
+        documentaryResourceDAO.save(documentaryResource);
+    }
+
+    @Secured("BotUser")
+    public void deleteDocumentaryResource(@SecuredOperation("botAdm") final Chatbot bot, final DocumentaryResource documentaryResource) {
+        documentaryResourceDAO.delete(documentaryResource.getDreId());
+        if(documentaryResource.getAttId() != null){
+            attachmentServices.delete(bot, documentaryResource.getAttId());
+        }
+    }
+
+    public DocumentaryResource getNewdocumentaryResource(final Long botId) {
+        DocumentaryResource documentaryResource = new DocumentaryResource();
+        documentaryResource.setBotId(botId);
+        return documentaryResource;
     }
 }
