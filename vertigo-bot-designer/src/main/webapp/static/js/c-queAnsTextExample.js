c282172d668eb6f3bfc86e764c6e1b5eed0c1868
cmazereau <christelle.mazereau@kleegroup.com>
2024-11-12 16:18:56 +0100

    [Vertigo 4 Migration] Components `c-chatbotTextExample` and `c-queAnsTextExample`:       - Rename prop "value" to "modelValue" (so that "v-model" can be used)       - Replace calls to "Vue.component()" by calls to "Vue.defineComponent()".

--- vertigo-bot-designer/src/main/webapp/static/js/c-queAnsTextExample.js
+++ vertigo-bot-designer/src/main/webapp/static/js/c-queAnsTextExample.js
@@ -1,25 +1,29 @@
-Vue.component('c-queanstextexample', {
+window.addEventListener('vui-before-plugins', function (event) {
+    let vuiQueAnsTextExample = Vue.defineComponent({
 
-    props : {
-        value:    { type: String,  required: true },
-        modeEdit: { type: Boolean, 'default': true },
-        locale:   { type: String, 'default': 'en_US' },
-        textColor: {type: String, 'default': '#000'},
-        error : false
-    },
-
-    template : `
-	<div style="width:300px;" class="q-px-md">
-		<q-chat-message :sent="false" :text="getChatPreview()" :text-color="textColor" bg-color="grey-4" ></q-chat-message>
-	</div>
-	`
-    ,
-    methods: {
-        getChatPreview: function() {
-            return !this.value ? [''] :
-                DOMPurify.sanitize(this.value)
-                    .replace("<a ", "<a target='_blank' rel='nofollow noopener noreferrer' ")
-                    .split(/<hr>|<hr \/>/);
+        props: {
+            modelValue: {type: String, required: true},
+            modeEdit: {type: Boolean, 'default': true},
+            locale: {type: String, 'default': 'en_US'},
+            textColor: {type: String, 'default': '#000'},
+            error: false
         },
-    }
+
+        template: `
+        <div style="width:300px;" class="q-px-md">
+            <q-chat-message :sent="false" :text="getChatPreview()" :text-color="textColor" bg-color="grey-4" ></q-chat-message>
+        </div>
+        `
+        ,
+        methods: {
+            getChatPreview: function () {
+                return !this.modelValue ? [''] :
+                    DOMPurify.sanitize(this.modelValue)
+                        .replace("<a ", "<a target='_blank' rel='nofollow noopener noreferrer' ")
+                        .split(/<hr>|<hr \/>/);
+            },
+        }
+    });
+
+    event.detail.vuiAppInstance.component('c-queanstextexample', vuiQueAnsTextExample);
 });
