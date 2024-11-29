11296241e5319726e1fef95fa2a0f3444cacdf07
cmazereau <christelle.mazereau@kleegroup.com>
2024-11-29 16:37:50 +0100

    [CHATBOT-628] Type operators label correction.

--- vertigo-bot-designer/src/main/resources/sql/v_2_0/create_24_07_24.sql
+++ vertigo-bot-designer/src/main/resources/sql/v_2_0/create_24_07_24.sql
@@ -22,8 +22,8 @@ comment on column TYPE_OPERATOR.LABEL_FR is
 -- ============================================================
 --   Insert MasterData values : TYPE_OPERATOR
 -- ============================================================
-insert into TYPE_OPERATOR(TYOP_CD, LABEL, LABEL_FR) values ('EQUAL', 'Equal', 'Égal');
-insert into TYPE_OPERATOR(TYOP_CD, LABEL, LABEL_FR) values ('CONTAIN', 'Contain', 'Contient');
+insert into TYPE_OPERATOR(TYOP_CD, LABEL, LABEL_FR) values ('EQUALS', 'Equals', 'Égal');
+insert into TYPE_OPERATOR(TYOP_CD, LABEL, LABEL_FR) values ('CONTAINS', 'Contains', 'Contient');
 
 
 ALTER TABLE CONTEXT_POSSIBLE_VALUE
