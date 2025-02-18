0141a61f15b8f3049081dc0fb70dcc83b35db418
cmazereau <christelle.mazereau@kleegroup.com>
2025-02-18 16:35:44 +0100

    Anonymization script : replace Confluence and Jira URLs.

--- vertigo-bot-docker/devDependencies/restore/anonymize.sql
+++ vertigo-bot-docker/devDependencies/restore/anonymize.sql
@@ -1,3 +1,14 @@
 UPDATE chatbot_node SET url = 'http://localhost:8080/vertigo-bot-executor';
 UPDATE chatbot_node SET api_key = 'MyNodeApiKey!';
 UPDATE person SET login = REGEXP_REPLACE (login, '(?<=\@).*', 'mail.com');
+
+-- Paramétrage des connecteurs Confluence et Jira
+UPDATE confluence_setting
+   SET url = 'https://confluence.rct01.kleegroup.com/display/CHATBOTPOC/POC+Chatbot+Home',
+       login = 'achatbot',
+       password = 'x!Yw73=i5M_eM7';
+
+UPDATE jira_setting
+   SET url = 'https://jirasoft.rct01.kleegroup.com/projects/CHATBOTPOC/issues/?filter=allopenissues',
+       login = 'achatbot',
+       password = 'x!Yw73=i5M_eM7';
